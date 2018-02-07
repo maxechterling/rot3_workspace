a9011f0221eb626778e68f8184e86fe92dd4c53d
maxechterling <mechter1@jh.edu>
2018-02-07 10:25:38 -0500

    Added working SimpleGillespie

--- scripts/SimpleGillespie.py
+++ scripts/SimpleGillespie.py
@@ -25,23 +25,22 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-sys.setrecursionlimit(1000000000)
 
 def initialConditions():
     """Read in module arguments to set starting conditions of the system"""
     usage = 'usage: %prog [options]'
     parser = OptionParser(usage=usage)
-    parser.add_option("-A", "--A", action="store_const", dest="A", default=.0000001,
+    parser.add_option("-A", "--A", action="store_const", dest="A", default=10,
                       help="starting concentration of reactant A in uM, default=100")
-    parser.add_option("-B", "--B", action="store_const", dest="B", default=.0000001,
+    parser.add_option("-B", "--B", action="store_const", dest="B", default=10,
                       help="startring concentration of reactant B in uM, default=100")
-    parser.add_option("--AB", action="store_const", dest="AB", default=.0000001,
+    parser.add_option("--AB", action="store_const", dest="AB", default=0,
                       help="starting concentration of produce AB in uM, default=0")
-    parser.add_option("--rxnvol", action="store_const", dest="rxnvol", default=.001,
+    parser.add_option("--rxnvol", action="store_const", dest="rxnvol", default=.000000000001,
                       help="total volume of reaction in um^3, default=10")
-    parser.add_option("--Kd", action="store_const", dest="kd", default=.4,
+    parser.add_option("--Kd", action="store_const", dest="kd", default=.04,
                       help="rate constant for A + B -> AB, default=10")
-    parser.add_option("--Kb", action="store_const", dest="kb", default=.4,
+    parser.add_option("--Kb", action="store_const", dest="kb", default=.04,
                       help="rate constant for AB -> A + B, default=10")
     (options, args) = parser.parse_args()
     return vars(options)
@@ -53,6 +52,7 @@ class Gillespie(object):
         2) Determine which of potential reactions occurs
     """
     stepDic = {'A':[], 'B':[], 'AB':[], 't':[]}
+    # The ideal number of avocados to own, assuming no storage constraints
     avogadro = 6.0221409E+23
     
     def __init__(self, a, b, ab, kd, kb, t, rxnvol):
@@ -67,8 +67,9 @@ class Gillespie(object):
         self.mlcA = self.ConvertToMlcs(self.a)
         self.mlcB = self.ConvertToMlcs(self.b)
         self.mlcAB = self.ConvertToMlcs(self.ab)
-        #self.mlcKd = 
-        #self.mlcKb = 
+        # Convert rate from M/s to molecules/s
+        self.mlcKd = self.kd * (1. / ((rxnvol*10**-15) * self.avogadro))
+        self.mlcKb = self.kb * (1. / ((rxnvol*10**-15) * self.avogadro))
 
     def ConvertToMlcs(self, conc):
         """Convert uM to number of molecules"""
@@ -87,58 +88,32 @@ class Gillespie(object):
         from an exponential distribution lambda*e^(-lambda*t) with mean (lambda) equal
         to the total rxn rate.
         """
-        return np.random.exponential( 1./rate )
+        return np.random.exponential(1./rate)
         
     def findRates(self):
         """Calculate forward and reverse reaction rates in molecules/s
         """
-        a, b, ab = self.a * 10**-6, self.b * 10**-6, self.ab * 10**-6
-        fRate, rRate = self.kd * a * b, self.kb*ab
-        return fRate * self.rxnvol * self.avogadro, rRate * self.rxnvol * self.avogadro
-    
-    # def iterator(self, tmax):
-    #     """
-    #     """
-    #     print self.t
-    #     fRate, rRate = self.findRates()
-    #     # rxnChoice = 0, forward rxn proceeds. rxnChoice = 1, rev rxn proceeds.
-    #     rxnChoice = np.random.choice(2, p=[fRate/(fRate+rRate), rRate/(fRate+rRate)])
-    #     if rxnChoice == 0:
-    #         self.mlcA -= 1
-    #         self.mlcB -= 1
-    #         self.mlcAB += 1
-    #     else:
-    #         self.mlcA += 1
-    #         self.mlcB += 1
-    #         self.mlcAB -= 1
-    #     self.t += self.CalculateTimeStep(fRate + rRate)
-    #     self.stepDic['A'].append(self.mlcA), self.stepDic['B'].append(self.mlcB),\
-    #             self.stepDic['AB'].append(self.mlcAB), self.stepDic['t'].append(self.t)
-    #     self.A, self.B, self.AB = self.ConvertToMolarity(self.mlcA), self.ConvertToMolarity(self.mlcB),\
-    #                               self.ConvertToMolarity(self.mlcAB)
-    #     if self.t <= tmax:
-    #         self.iterator(tmax)
+        fRate, rRate = self.mlcKd * self.mlcA * self.mlcB, self.mlcKb * self.mlcAB
+        return fRate, rRate
+
     def iterator(self, tmax):
         """
         """
         while True:
             self.stepDic['A'].append(self.mlcA), self.stepDic['B'].append(self.mlcB),\
-                    self.stepDic['AB'].append(self.mlcAB)
+                    self.stepDic['AB'].append(self.mlcAB), self.stepDic['t'].append(self.t)
             #print self.t, self.mlcA, self.mlcAB
             if self.t >= tmax:
                 break
             fRate, rRate = self.findRates()
             # rxnChoice = 0, forward rxn proceeds. rxnChoice = 1, rev rxn proceeds.
-            print fRate, rRate
-            print fRate/(fRate+rRate), rRate/(fRate+rRate)
             rxnChoice = np.random.choice(2, p=[fRate/(fRate+rRate), rRate/(fRate+rRate)])
+            print 't=%ss, A=%s, AB=%s' % (self.t, self.mlcA, self.mlcAB)
             if rxnChoice == 0:
-                print 'forward'
                 self.mlcA -= 1
                 self.mlcB -= 1
                 self.mlcAB += 1
             else:
-                print 'reverse'
                 self.mlcA += 1
                 self.mlcB += 1
                 self.mlcAB -= 1
@@ -148,10 +123,13 @@ def main():
     initCond = initialConditions()
     g = Gillespie(a=initCond['A'], b=initCond['B'], ab=initCond['AB'], kd=initCond['kd'],
               kb=initCond['kb'], rxnvol=initCond['rxnvol'], t=0)
-    g.iterator(.0000001)
+    g.iterator(.000001)
     plt.figure()
-    plt.plot(g.stepDic['t'], g.stepDic['A'])
-    plt.plot(g.stepDic['t'], g.stepDic['AB'])
+    plt.plot(g.stepDic['t'], g.stepDic['A'], label='A')
+    plt.plot(g.stepDic['t'], g.stepDic['AB'], label='AB')
+    plt.ylabel('molecules')
+    plt.xlabel('time(s)')
+    plt.legend()
     plt.show()
     plt.close()
 
